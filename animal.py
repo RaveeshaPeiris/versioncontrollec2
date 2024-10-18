7c4f45cb6f5ff5a46d8e34ec5d46867ff0663c69
RaveeshaPeiris <rpeiris4444@gmail.com>
2024-10-18 15:35:57 +0530

    cat

--- animal.py
+++ animal.py
@@ -1,10 +1,19 @@
 import sys
 
+def cat():
+    print("meow")
+
 def default():
     print('hello')
 
 def main () :
     default()
+
+def main():
+    if sys.argv[1] == 'cat':
+        cat()
+    else:
+        default()
 
 if __name__ == '__main__' :
     main()
